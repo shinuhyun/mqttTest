e5714615684fe123fbba944980ae0a4955c4252c
shinuhyun <shinuhyun@gmail.com>
2022-11-28 16:20:32 +0900

    [feat]: Publish message

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 import express from "express";
+import mqtt from "mqtt";
+import cors from "cors";
+
 const app = express();
 const port = 3000;
 
 import path from "path";
+
 import { fileURLToPath } from "url";
+import { json } from "stream/consumers";
+
+const corsOption = {
+  origin: "*",
+  method: "GET,POST,PUT,PATCH,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(cors(corsOption));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -19,6 +34,22 @@ app.get("/css", (req, res) => {
 
 app.get("/js", (req, res) => {
   res.sendFile(__dirname + "/main.js");
+});
+
+app.post("/mqtt", async (req, res) => {
+  console.log(req.body);
+  const { pub_ip, pub_topic, pub_message } = req.body;
+  console.log("server", { pub_ip, pub_topic, pub_message });
+
+  const client = mqtt.connect(`mqtt://${pub_ip}`);
+  client.publish(pub_topic, pub_message);
+  client.publish(pub_topic, "lelele");
+  client.on("message", function (pub_topic, message) {
+    // message is Buffer
+    console.log(message.toString());
+    client.end();
+  });
+  res.status(200).json({ pub_message });
 });
 
 app.listen(port, () => {
--- main.js
+++ main.js
@@ -1,27 +1,22 @@
 console.log("THIS IS SERVER!!!");
-import mqtt from "/mqtt";
 
 const pubBtn = document.querySelector("#publishBtn");
-const client = mqtt.connect("mqtt://test.mosquitto.org");
-
-client.publish("presence", "Hello mqtt");
 
 pubBtn.addEventListener("click", publishMessage);
 
-function publishMessage() {
-  console.log("publish!");
+async function publishMessage(e) {
+  const pub_ip = document.querySelector("#pub_ip").value;
+  const pub_topic = document.querySelector("#pub_topic").value;
+  const pub_message = document.querySelector("#pub_message").value;
+  console.log({ pub_ip, pub_topic, pub_message });
+  await fetch("/mqtt", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ pub_ip, pub_topic, pub_message }),
+  })
+    .then((res) => res.json())
+    .then(console.log)
+    .catch(console.error);
 }
-
-// client.on("connect", function () {
-//   client.subscribe("presence", function (err) {
-//     if (!err) {
-//       client.publish("presence", "Hello mqtt");
-//     }
-//   });
-// });
-
-// client.on("message", function (topic, message) {
-//   // message is Buffer
-//   console.log(message.toString());
-//   client.end();
-// });
